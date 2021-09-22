373df674d2788f5a349f92ce179fc0ee186a6143
jgurr4 <jared@gurrfamily.com>
2021-09-22 14:03:27 -0700

    Successfully outputted dissected values to index.html page as json object.

--- src/main/resources/static/result.js
+++ src/main/resources/static/result.js
@@ -1,3 +1,16 @@
+function showDissectedResults(data) {
+    console.log(data)
+    const affixMap = data.affixMap
+    const term = data.term
+    const definition = data.definition
+    const jsonAffixes = JSON.stringify(affixMap)
+    console.log(jsonAffixes)
+    const resultsDiv = document.getElementById("results");
+    const affixMapElement = document.createElement("p");
+    // affixMapElement.innerText = `${data.affixMap.algesi[0].affix}`;
+    affixMapElement.innerText = `${JSON.stringify(data)}`;
+    resultsDiv.append(affixMapElement);
+}
 
 function obtainJson() {
     let inputTerm = document.getElementById("inputTerm").value
@@ -11,5 +24,7 @@ function obtainJson() {
         })
     }).then(res => {
         return res.json()
-    }).then(data => console.log(data))
+    }).then(data => {
+        showDissectedResults(data)
+    })
 }
