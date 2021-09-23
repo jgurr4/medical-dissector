4cde55d81a115835f7238dc33043e5b3822a1d30
jgurr4 <jared@gurrfamily.com>
2021-09-22 18:50:45 -0700

    Successfully split dissected object into parts in console. Next step to display in html page.

--- src/main/resources/static/result.js
+++ src/main/resources/static/result.js
@@ -3,11 +3,25 @@ function showDissectedResults(data) {
     const affixMap = data.affixMap
     const term = data.term
     const definition = data.definition
-    const jsonAffixes = JSON.stringify(affixMap)
-    console.log(jsonAffixes)
     const resultsDiv = document.getElementById("results");
     const affixMapElement = document.createElement("p");
     // affixMapElement.innerText = `${data.affixMap.algesi[0].affix}`;
+    const keys = Object.keys(affixMap);
+    console.log(keys)
+    for (let i = 0; i < keys.length; i++) {
+        console.log(keys[i])
+        let key = keys[i]
+        // console.log(affixMap[`${key}`])
+        if (affixMap[`${key}`] !== null) {
+            for (let j = 0; j < affixMap[`${key}`].length; j++) {
+                console.log(affixMap[`${key}`][j].affix)
+                console.log(affixMap[`${key}`][j].meaning)
+                console.log(affixMap[`${key}`][j].examples)
+            }
+        } else {
+            console.log(key + " is null")
+        }
+    }
     affixMapElement.innerText = `${JSON.stringify(data)}`;
     resultsDiv.append(affixMapElement);
 }
